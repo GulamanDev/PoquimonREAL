c005e385d19a560d799559c1a5f5132cc5c4a81d
JmBears <115405999+JmBears@users.noreply.github.com>
2024-02-24 19:53:40 +0800

    Player Movement + Animations [WORKING]

--- 2D RPG/Assets/Scripts/Player/PlayerMovement.cs
+++ 2D RPG/Assets/Scripts/Player/PlayerMovement.cs
@@ -9,18 +9,27 @@ public class PlayerMovement : MonoBehaviour
     private Vector2 _movement;
 
     private Rigidbody2D _rb;
+    private Animator _animator;
+
+    //  const strings for ANIMATOR PARAMETERS, so they can't be changed during runtime
+    private const string _horizontal = "Horizontal";
+    private const string _vertical = "Vertical";
 
     private void Awake()
     {
         _rb = GetComponent<Rigidbody2D>();
+        _animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         _movement.Set(InputManager.Movement.x, InputManager.Movement.y);
 
-        _rb.velocity = _movement * _moveSpeed;
         //  Since we're setting the velocity directly, we don't need to add deltaTime or
         //  even FixedUpdate, since we're overwriting everything with velocity.
+        _rb.velocity = _movement * _moveSpeed;
+
+        _animator.SetFloat(_horizontal, _movement.x);
+        _animator.SetFloat(_vertical, _movement.y);
     }
 }
