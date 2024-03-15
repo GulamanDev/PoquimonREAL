fb47e739ef7f1da2c560e093bf22885ab8783ae5
Gulaman Dev <justinapolonio2000@gmail.com>
2024-03-15 21:40:16 +0800

    Revert "Lots of updates refer to desc. WIP"
    This reverts commit 7129835c367c6261083a6c83220716d43b08049c.

--- 2D RPG/Assets/Scripts/DialogBox.cs
+++ 2D RPG/Assets/Scripts/DialogBox.cs
@@ -1,3 +1,4 @@
+// DialogBox.cs
 using UnityEngine;
 using TMPro;
 
@@ -5,22 +6,14 @@ public class DialogBox : MonoBehaviour
 {
     public TextMeshProUGUI dialogText;
 
-    // Method to check if the dialog box is active
-    public bool IsDialogActive()
-    {
-        // Return true if the dialog box game object is active
-        return gameObject.activeSelf;
-    }
-
-    // Method to show the dialog box
     public void ShowDialog(string text)
     {
         // Show the dialog box and set the text
+        // You can customize the appearance and behavior here
         gameObject.SetActive(true);
         dialogText.text = text;
     }
 
-    // Method to hide the dialog box
     public void HideDialog()
     {
         // Hide the dialog box
